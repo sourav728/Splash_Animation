80022df24efa34001e8d2d9f2b4129b69ee89b7b
Sourav Nandi <souravnandi728@outlook.com>
2019-04-23 18:18:15 +0530

    Initial commit

---
+++ app/src/main/java/com/tvd/splash_screen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tvd.splash_screen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
